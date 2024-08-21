82fd12d39d5d31b2a131beb274171688ee7e7d46
Yerkebulan <erkebulanice@gmail.com>
2024-08-21 15:05:21 +0500

    initilized

---
+++ bin/custom_bloc_template.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+import 'package:args/args.dart';
+
+void main(List<String> arguments) {
+  final parser = ArgParser()
+    ..addCommand('create-feature');
+
+  var argResults = parser.parse(arguments);
+
+  if (argResults.command?.name == 'create-feature') {
+    createFeatureTemplate(argResults.command?.rest[0] ?? 'new_feature');
+  } else {
+    print('Unknown command. Use "create-feature" to generate a new feature template.');
+  }
+}
+
+void createFeatureTemplate(String featureName) {
+  final featurePath = 'lib/features/$featureName';
+  final directories = [
+    '$featurePath/bloc',
+    '$featurePath/repository',
+    '$featurePath/view',
+  ];
+
+  for (var dir in directories) {
+    Directory(dir).createSync(recursive: true);
+    print('Created: $dir');
+  }
+  print('Feature template for "$featureName" created successfully.');
+}
